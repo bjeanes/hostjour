288a606d9ba32d6d39659ce14ba8e20151af8588
Bodaniel Jeanes <me@bjeanes.com>
2008-11-13 00:29:25 +1000

    playing around with DNSSD in Terminal to see how it works

--- lib/hostjour.rb
+++ lib/hostjour.rb
@@ -1,6 +1,28 @@
 $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
+require 'rubygems'
+require 'dnssd'
+
 module Hostjour
   VERSION = '0.0.1'
+  
+  SERVICE = "_http._tcp"
+  
+  def self.list
+    servers = {}
+    service = DNSSD.browse(SERVICE) do |reply|
+      servers[reply.name] ||= reply
+    end
+    STDERR.puts "Searching for servers (3 seconds)"
+    # Wait for something to happen
+    sleep 3
+    service.stop
+    servers.each { |string,obj|
+      name, port = string.split ":" 
+      STDERR.puts "Found web app called '#{name}'"
+    }
+  end
 end
+
+Hostjour.list
